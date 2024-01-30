a3610ffb609eb4adfa2ea81eaf78159e9154bb2a
Nate <npzaldivar@gmail.com>
2024-01-30 08:26:47 -0700

    Adding sprites and background to game

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
 function love.load()
+    -- Sprites Values
+    sprites = {}
+    sprites.background = love.graphics.newImage("sprites/background.png")
+    sprites.bullet = love.graphics.newImage("sprites/bullet.png")
+    sprites.player = love.graphics.newImage("sprites/player.png")
+    sprites.zombie = love.graphics.newImage("sprites/zombie.png")
 
+    -- Player Values
 end
 
 function love.update(dt)
@@ -7,5 +14,5 @@ function love.update(dt)
 end
 
 function love.draw()
-
+    love.graphics.draw(sprites.background, 0, 0)
 end
